0cb6269de81fdf4f9b0d742c26dadf73095d2110
JosMa30 <169048447+JosMa30@users.noreply.github.com>
2024-05-08 10:51:51 +0200

    Update estudiantes_y_asignaturas_subcadenas_ENUNCIADOS.sql
    Todo bien

--- estudiantes_y_asignaturas_subcadenas_ENUNCIADOS.sql
+++ estudiantes_y_asignaturas_subcadenas_ENUNCIADOS.sql
@@ -1,3 +1,5 @@
+Hoy es 8 de mayo y son las 10 y 50
+	
 CREATE DATABASE estudiantes_y_asignaturas_alter;
 GO
 USE estudiantes_y_asignaturas_alter;
@@ -31,27 +33,27 @@ CREATE TABLE asignaturas_matriculadas(
 
 
 --Insertar datos en tabla "estudiantes"
-INSERT INTO estudiantes values(1, 'Juan PÈrez' ,20, 'IngenierÌa de Sistemas' ,8.5)
-INSERT INTO estudiantes values(2, 'MarÌa GarcÌa', 22 , 'PsicologÌa' ,7.2)
-INSERT INTO estudiantes values(3, 'Carlos MartÌnez' ,21, 'AdministraciÛn de Empresas' ,9.5)
-INSERT INTO estudiantes values(4, 'SofÌa LÛpez' ,19, 'Contabilidad' ,5.5)
-INSERT INTO estudiantes values(5, 'Luis RodrÌguez' ,23, 'IngenierÌa Civil' ,8.7)
+INSERT INTO estudiantes values(1, 'Juan P√©rez' ,20, 'Ingenier√≠a de Sistemas' ,8.5)
+INSERT INTO estudiantes values(2, 'Mar√≠a Garc√≠a', 22 , 'Psicolog√≠a' ,7.2)
+INSERT INTO estudiantes values(3, 'Carlos Mart√≠nez' ,21, 'Administraci√≥n de Empresas' ,9.5)
+INSERT INTO estudiantes values(4, 'Sof√≠a L√≥pez' ,19, 'Contabilidad' ,5.5)
+INSERT INTO estudiantes values(5, 'Luis Rodr√≠guez' ,23, 'Ingenier√≠a Civil' ,8.7)
 INSERT INTO estudiantes values(6, 'Ana Ruiz', 20, 'Medicina', 9.2)
-INSERT INTO estudiantes values(7, 'Pablo GÛmez', 22, 'Arquitectura' ,8.1)
-INSERT INTO estudiantes values(8, 'Andrea DÌaz', 21, 'EconomÌa', 6.7)
-INSERT INTO estudiantes values(9, 'Elena Fern·ndez', 20, 'PsicologÌa', 7.3)
-INSERT INTO estudiantes values(10, 'Miguel Torres', 27, 'EconomÌa', 8.2)
+INSERT INTO estudiantes values(7, 'Pablo G√≥mez', 22, 'Arquitectura' ,8.1)
+INSERT INTO estudiantes values(8, 'Andrea D√≠az', 21, 'Econom√≠a', 6.7)
+INSERT INTO estudiantes values(9, 'Elena Fern√°ndez', 20, 'Psicolog√≠a', 7.3)
+INSERT INTO estudiantes values(10, 'Miguel Torres', 27, 'Econom√≠a', 8.2)
 --Insertar datos en tabla "asignaturas"
-INSERT INTO asignaturas VALUES (1,'Bases de Datos',3,'Ciencias de la ComputaciÛn')
-INSERT INTO asignaturas VALUES (2,'Matem·ticas Avanzadas',4,'Matem·ticas')
-INSERT INTO asignaturas VALUES (3,'Finanzas Corporativas',3,'EconomÌa')
-INSERT INTO asignaturas VALUES (4,'PsicologÌa Social',3,'PsicologÌa')
-INSERT INTO asignaturas VALUES (5,'InglÈs Avanzado',2,'Lenguas Extranjeras')
-INSERT INTO asignaturas VALUES (6, 'ProgramaciÛn en C', 4, 'Ciencias de la ComputaciÛn')
-INSERT INTO asignaturas VALUES (7, 'Arte Contempor·neo', 2, 'Bellas Artes')
+INSERT INTO asignaturas VALUES (1,'Bases de Datos',3,'Ciencias de la Computaci√≥n')
+INSERT INTO asignaturas VALUES (2,'Matem√°ticas Avanzadas',4,'Matem√°ticas')
+INSERT INTO asignaturas VALUES (3,'Finanzas Corporativas',3,'Econom√≠a')
+INSERT INTO asignaturas VALUES (4,'Psicolog√≠a Social',3,'Psicolog√≠a')
+INSERT INTO asignaturas VALUES (5,'Ingl√©s Avanzado',2,'Lenguas Extranjeras')
+INSERT INTO asignaturas VALUES (6, 'Programaci√≥n en C', 4, 'Ciencias de la Computaci√≥n')
+INSERT INTO asignaturas VALUES (7, 'Arte Contempor√°neo', 2, 'Bellas Artes')
 INSERT INTO asignaturas VALUES (8, 'Derecho Civil', 3, 'Derecho')
-INSERT INTO asignaturas VALUES (9, 'Marketing EstratÈgico', 3, 'Marketing')
-INSERT INTO asignaturas VALUES (10, 'FÌsica Cu·ntica', 4, 'FÌsica')
+INSERT INTO asignaturas VALUES (9, 'Marketing Estrat√©gico', 3, 'Marketing')
+INSERT INTO asignaturas VALUES (10, 'F√≠sica Cu√°ntica', 4, 'F√≠sica')
 --Insertar datos en tabla "asignaturas_matriculadas"
 INSERT INTO asignaturas_matriculadas values (1, 1);
 INSERT INTO asignaturas_matriculadas values (1, 6);
@@ -75,13 +77,13 @@ SELECT * FROM asignaturas_matriculadas
 -- Ejercicios con subcadenas (las estructuras de las tablas y los datos estan en este mismo archivo)
 
 -- 1. Obtener el nombre de los estudiantes seguido de su edad, con este formato: 
--- "Juan PÈrez tiene 20 aÒos"
+-- "Juan P√©rez tiene 20 a√±os"
 
 
 -- 2. Obtener el nombre de los estudiantes seguido de su carrera y su nota media, con este formato: 
--- "Juan PÈrez estudia IngenierÌa de Sistemasy tiene una media de 8.5"
+-- "Juan P√©rez estudia Ingenier√≠a de Sistemasy tiene una media de 8.5"
 
 
--- 3. Obtener el nombre de los estudiantes seguido del n∫ de asignaturas que tiene cada uno
--- "Juan PÈrez tiene 3 asignaturas"
+-- 3. Obtener el nombre de los estudiantes seguido del n¬∫ de asignaturas que tiene cada uno
+-- "Juan P√©rez tiene 3 asignaturas"
 
